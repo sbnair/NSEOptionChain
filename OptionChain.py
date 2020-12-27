592fc155925cf636fa93629b093ea5cb2e3cf880
Thanuj D R <thanujdr@hotmail.com>
2020-12-27 23:06:19 +0530

    Working model of Option Chain is ready. Output will be a .html file

--- OptionChain.py
+++ OptionChain.py
@@ -18,7 +18,6 @@ class NSE:
         __init__ of NSE class
         '''
         self.cookies = None
-        # self.request = None
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
             'accept-encoding': 'gzip, deflate, br',
@@ -145,58 +144,134 @@ class NSE:
         '''
         try:
             request = self.get_nse_data(type=type, symbol=symbol)
+
             if expiry:
                 ce_data = [data['CE'] for data in request['records']['data'] if "CE" in data and str(data['expiryDate']).lower() == str(expiry).lower()]
                 pe_data = [data['PE'] for data in request['records']['data'] if "PE" in data and str(data['expiryDate']).lower() == str(expiry).lower()]
             else:
+                expiry = request['filtered']['data'][0]['expiryDate']
                 ce_data = [data['CE'] for data in request['filtered']['data'] if "CE" in data]
                 pe_data = [data['PE'] for data in request['filtered']['data'] if "PE" in data]
 
             ce_df = pd.DataFrame(ce_data)
             ce_df = ce_df.drop(['expiryDate', 'underlying', 'identifier', 'bidQty', 'bidprice', 'askQty', 'askPrice', 'changeinOpenInterest', 'change', 'underlyingValue'], axis = 1) 
-            ce_df = ce_df[['totalBuyQuantity', 'totalSellQuantity', 'totalTradedVolume', 'pchangeinOpenInterest', 'openInterest', 'lastPrice', 'pChange', 'impliedVolatility', 'strikePrice']]
-            ce_df = ce_df.rename(columns={'totalBuyQuantity':'BuyQuantity', 'totalSellQuantity':'SellQuantity', 'totalTradedVolume':'TotalVolume', 'pchangeinOpenInterest':'poi', 'openInterest':'oi', 'lastPrice':'ltp', 'pChange':'pltp', 'impliedVolatility':'iv'})
+            ce_df = ce_df[['totalBuyQuantity', 'totalSellQuantity', 'totalTradedVolume', 'openInterest', 'pchangeinOpenInterest', 'lastPrice', 'pChange', 'impliedVolatility', 'strikePrice']]
+            ce_df = ce_df.rename(columns={'totalBuyQuantity':'BuyQ', 'totalSellQuantity':'SellQ', 'totalTradedVolume':'TotalVol', 'pchangeinOpenInterest':'pOI', 'openInterest':'OI', 'lastPrice':'LTP', 'pChange':'pLTP', 'impliedVolatility':'IV'})
 
             pe_df = pd.DataFrame(pe_data)
             pe_df= pe_df.drop(['expiryDate', 'underlying', 'identifier', 'bidQty', 'bidprice', 'askQty', 'askPrice', 'underlyingValue', 'changeinOpenInterest', 'change'], axis = 1)
-            pe_df = pe_df[['strikePrice', 'impliedVolatility', 'pChange', 'lastPrice', 'openInterest', 'pchangeinOpenInterest', 'totalTradedVolume', 'totalSellQuantity', 'totalBuyQuantity']]            
-            pe_df = pe_df.rename(columns={'totalBuyQuantity':'BuyQuantity', 'totalSellQuantity':'SellQuantity', 'totalTradedVolume':'TotalVolume', 'pchangeinOpenInterest':'poi', 'openInterest':'oi', 'lastPrice':'ltp', 'pChange':'pltp', 'impliedVolatility':'iv'})
+            pe_df = pe_df[['strikePrice', 'impliedVolatility', 'pChange', 'lastPrice', 'pchangeinOpenInterest', 'openInterest', 'totalTradedVolume', 'totalBuyQuantity', 'totalSellQuantity']]            
+            pe_df = pe_df.rename(columns={'totalBuyQuantity':'BuyQ', 'totalSellQuantity':'SellQ', 'totalTradedVolume':'TotalVol', 'pchangeinOpenInterest':'pOI', 'openInterest':'OI', 'lastPrice':'LTP', 'pChange':'pLTP', 'impliedVolatility':'IV'})
 
-            df = pd.merge(ce_df, pe_df, on='strikePrice', suffixes=('_ce', '_pe'), how='outer', sort=True).fillna(0)
-            df = df.astype({'BuyQuantity_ce':'uint64', 'SellQuantity_ce':'uint64', 'TotalVolume_ce':'uint64', 'oi_ce':'uint64', 'BuyQuantity_pe':'uint64', 'SellQuantity_pe':'uint64', 'TotalVolume_pe':'uint64', 'oi_pe':'uint64'})
-            df = df.astype({'poi_ce':'float', 'pltp_ce':'float', 'iv_ce':'float', 'poi_pe':'float', 'pltp_pe':'float', 'iv_pe':'float'}).round({'poi_ce':1, 'pltp_ce':1, 'iv_ce':1, 'poi_pe':1, 'pltp_pe':1, 'iv_pe':1})
-            df = df.astype({'ltp_ce':'float', 'ltp_pe':'float', 'strikePrice':'float'})
+            df = pd.merge(ce_df, pe_df, on='strikePrice', suffixes=('_c', '_p'), how='outer', sort=True).fillna(0)
+            df = df.astype({'BuyQ_c':'uint64', 'SellQ_c':'uint64', 'TotalVol_c':'uint64', 'OI_c':'uint64', 'BuyQ_p':'uint64', 'SellQ_p':'uint64', 'TotalVol_p':'uint64', 'OI_p':'uint64'})
+            df = df.astype({'pOI_c':'float', 'pLTP_c':'float', 'IV_c':'float', 'pOI_p':'float', 'pLTP_p':'float', 'IV_p':'float'}).round({'pOI_c':1, 'pLTP_c':1, 'IV_c':1, 'pOI_p':1, 'pLTP_p':1, 'IV_p':1})
+            df = df.astype({'LTP_c':'float', 'LTP_p':'float', 'strikePrice':'float'})
             
-            def cumm_ce(expiryPrice):
+            def MaxPain_c(expiryPrice):
                 sum = 0
                 for index in df.index:
                     if (expiryPrice - df['strikePrice'][index]) >= 0:
-                        sum = sum + (expiryPrice - df['strikePrice'][index]) * df['oi_ce'][index]  
+                        sum = sum + (expiryPrice - df['strikePrice'][index]) * df['OI_c'][index]  
                 return sum
 
-            def cumm_pe(expiryPrice):
+            def MaxPain_p(expiryPrice):
                 sum = 0
                 for index in df.index:
                     if (df['strikePrice'][index] - expiryPrice) >= 0:
-                        sum = sum + (df['strikePrice'][index] - expiryPrice) * df['oi_pe'][index]  
+                        sum = sum + (df['strikePrice'][index] - expiryPrice) * df['OI_p'][index]  
                 return sum
                                       
-            df['cumm_ce'] = df['strikePrice'].apply(lambda row: cumm_ce(row))
-            df['cumm_pe'] = df['strikePrice'].apply(lambda row: cumm_pe(row))
-            df['cumm'] = df['cumm_ce'] + df['cumm_pe']  # df['cumm'] = df.apply(lambda row: row['cumm_ce'] + row['cumm_pe'], axis = 1)
-            df = df.drop(['cumm_ce', 'cumm_pe'], axis=1)
-            maxpain = df.loc[df['cumm'].idxmin()]['strikePrice']
-            
-            
-            pcr_oi = df['oi_pe'].sum()/df['oi_ce'].sum()
-            pcr_vol = df['TotalVolume_pe'].sum()/df['TotalVolume_ce'].sum()
+            df['MaxPain_c'] = df['strikePrice'].apply(lambda row: MaxPain_c(row))
+            df['MaxPain_p'] = df['strikePrice'].apply(lambda row: MaxPain_p(row))
+            df['MaxPain'] = df['MaxPain_c'] + df['MaxPain_p']  # df['MaxPain'] = df.apply(lambda row: row['MaxPain_c'] + row['MaxPain_p'], axis = 1)
+            # df = df.drop(['MaxPain_c', 'MaxPain_p'], axis=1)
+            maxpain = df.loc[df['MaxPain'].idxmin()]['strikePrice']
+                        
+            pcr_oi = df['OI_p'].sum()/df['OI_c'].sum()
+            pcr_vol = df['TotalVol_p'].sum()/df['TotalVol_c'].sum()
 
             timestamp = request["records"]["timestamp"]
             underlyingValue = request["records"]["underlyingValue"]
 
-            return df, round(pcr_oi, 2), round(pcr_vol, 2), maxpain, timestamp, underlyingValue
+            return df, round(pcr_oi, 2), round(pcr_vol, 2), maxpain, timestamp, underlyingValue, expiry
         except Exception as err:
             print( "get_oc_data: ", err)
+
+    def get_output(self, type='indices', symbol='NIFTY', expiry=None):
+        df, pcr_oi, pcr_vol, maxpain, timestamp, underlyingValue, exp = self.get_oc_data(type=type, symbol=symbol, expiry=expiry)   
+
+        def color_pcolumn(val):            
+            """
+            Takes a scalar and returns a string with
+            the css property `'color: red'` for negative
+            strings, black otherwise.
+            """
+            color = 'red' if val < 0 else 'green'
+            return 'color: %s' % color
+
+        # def color_background(val):
+        #     return 'background-color':'yellow'
+        
+        def highlight_min(data, color='#df66e8'):
+            '''
+            highlight the minimum in a Series or DataFrame
+            '''
+            attr = 'background-color: {}'.format(color)
+            if data.ndim == 1:  # Series from .apply(axis=0) or axis=1
+                is_min = data == data.min()
+                return [attr if v else '' for v in is_min]
+            else:  # from .apply(axis=None)
+                is_min = data == data.min().min()
+                return pd.DataFrame(np.where(is_min, attr, ''),
+                                    index=data.index, columns=data.columns)
+
+        atm_index = df['strikePrice'].sub(underlyingValue).abs().idxmin()
+        
+        df = df.iloc[atm_index-15:atm_index+15,:]
+
+        df = df.drop(['MaxPain_c', 'MaxPain_p', 'IV_c', 'IV_p'], axis=1)
+        df_style = df.style    
+
+        # def hover(hover_color="blue"):
+        #     return dict(selector="tr:hover",
+        #                 props=[("background-color", "%s" % hover_color)])
+
+        styles = [
+            # hover(),
+            dict(selector="th", props=[("font-size", "100%"), ("text-align", "center"), ('background-color', 'grey')]),
+            dict(selector="caption", props=[("caption-side", "top"), ("font-size", "120%"), ("font-family", "roboto"), ("text-align", "center")])
+        ]
+        temp = str(symbol) + "<br />Expiry - " + str(exp) + "<br /><br />" + "UnderlyingValue - " + \
+            str(underlyingValue)+ "&nbsp;&nbsp;&nbsp;&nbsp;PCR_OI - " + str(pcr_oi) + \
+                "&nbsp;&nbsp;&nbsp;&nbsp;PCR_Vol - " + str(pcr_vol) + "<br /><br />Last updated - " + str(timestamp)
+        df_style = (df_style.set_table_styles(styles).set_caption(temp))
+
+
+        df_style = df_style.bar(subset=['OI_c'], color='#eb6e6e')
+        df_style = df_style.bar(subset=['OI_p'], color='#4fc95b')
+        df_style = df_style.bar(subset=['MaxPain'], color='#df66e8')
+        df_style = df_style.bar(subset=['TotalVol_c', 'TotalVol_p'], color='#26abad')
+        # df_style = df_style.bar(subset=['pOI_c', 'pOI_p'], align='mid', color=['#eb6e6e', '#4fc95b'])
+        df_style = df_style.set_properties(**{'width':'80px', 'color':'black', 'font-family':'roboto', 'text-align':'center', 'font-weight':'400'})
+        df_style = df_style.set_properties(**{'background-color':'#f7f7f7'})
+        df_style = df_style.set_properties(**{'background-color':'#ddd5de'}, subset=['strikePrice'])
+        df_style = df_style.applymap(lambda x: 'background-color:#f5f4cb', subset=pd.IndexSlice[:atm_index,['BuyQ_c', 'SellQ_c', 'TotalVol_c', 'OI_c', 'pOI_c', 'LTP_c', 'pLTP_c']])
+        df_style = df_style.applymap(lambda x: 'background-color:#f5f4cb', subset=pd.IndexSlice[atm_index:,['BuyQ_p', 'SellQ_p', 'TotalVol_p', 'OI_p', 'pOI_p', 'LTP_p', 'pLTP_p']])
+        df_style = df_style.apply(highlight_min, subset=['MaxPain'])
+        df_style = df_style.set_properties(**{'width':'300px'}, subset=['OI_c', 'OI_p'])
+        df_style = df_style.set_properties(**{'width':'200px'}, subset=['TotalVol_c', 'TotalVol_p'])
+        df_style = df_style.applymap(color_pcolumn, subset=pd.IndexSlice[:,['pOI_c', 'pOI_p', 'pLTP_c', 'pLTP_p']])
+
+        df_style = df_style.hide_index()
+        # df_style = df_style.set_precision(2)
+        df_style = df_style.format({'pOI_c':'{:.1f}', 'pLTP_c':'{:.1f}',  \
+            'pOI_p':'{:.1f}', 'pLTP_p':'{:.1f}', 'LTP_c':'{:.2f}', 'LTP_p':'{:.2f}', \
+                'strikePrice':'{:.2f}', 'MaxPain':'{:.0f}'})   
+        html = df_style.render()  
+        
+        with open("index.html","w") as f:
+            f.write(html)
 
     def save_data(self, data, path):
         '''
@@ -281,26 +356,10 @@ class NSE:
 
 if __name__ == "__main__":
     nse = NSE()
-    df, pcr_oi, pcr_vol, maxpain, timestamp, underlyingValue = nse.get_oc_data(expiry='31-Dec-2020') 
-    
-    print(df.head())
-    print(pcr_oi, pcr_vol, maxpain, sep=' - ')
-    print(type(underlyingValue))
- 
-    
-    # df = df.style.set_precision(2)
-#     df.style.format({
-#     'A': '{:,.1f}'.format,
-#     'B': '{:,.3f}'.format,
-# })
-
-    df_style = df.style.bar(subset=['oi_ce'], color='red')
-    df_style = df_style.bar(subset=['cumm'], color='grey')
-    html = df_style.set_precision(2).hide_index().render()  
-    # html = html.set_properties( **{'width': '200px', 'background-color': 'yellow', 'color': 'black', 'border-color': 'black'}).render() 
-    
-    with open("index.html","w") as f:
-        f.write(html)
+    symbol='NIFTY'
+    type1 = 'indices'  #equities/indices
+    expiry = '31-Dec-2020'
+    nse.get_output(symbol=symbol, type=type1, expiry=expiry)
 
 
     # with open('request.txt', 'w')as f:
@@ -320,5 +379,9 @@ if __name__ == "__main__":
                 # else:
                 
 
-# with open("test1.html", 'w') as f:
+    # with open("test1.html", 'w') as f:
             #     f.write(df.to_html(index=False))
+
+    # df.style.format({'A': '{:.16f}', 'D': '{:.5f}'})
+
+    
